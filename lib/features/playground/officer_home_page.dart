f7802060343547452b76844e0dee8edb214289ef
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2024-12-10 11:12:36 +0800

    post updated

--- lib/features/playground/officer_home_page.dart
+++ lib/features/playground/officer_home_page.dart
@@ -1,86 +1,359 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+// import 'package:geolocation/core/globalwidget/sliver_gap.dart';
+// import 'package:geolocation/features/home/dashboard/widget/profile_section.dart';
+// import 'package:geolocation/features/post/controller/post_controller.dart';
+// import 'package:geolocation/features/post/model/post.dart';
+// import 'package:geolocation/features/post/widget/post_widget.dart';
+// import 'package:get/get.dart';
+// import 'package:sliver_tools/sliver_tools.dart';
+
+// class OfficerHomePage extends StatefulWidget {
+//   const OfficerHomePage({Key? key}) : super(key: key);
+
+//   @override
+//   State<OfficerHomePage> createState() => _OfficerHomePageState();
+// }
+
+// class _OfficerHomePageState extends State<OfficerHomePage>
+//     with SingleTickerProviderStateMixin {
+//   final PostController controller = Get.find<PostController>();
+//   final ScrollController newScrollController = ScrollController();
+
+//   late TabController tabController;
+
+//   @override
+//   void initState() {
+//     super.initState();
+
+//     // Initialize TabController
+//     tabController = TabController(length: 4, vsync: this);
+
+//     WidgetsBinding.instance.addPostFrameCallback((_) {
+//       controller.loadData();
+
+//       newScrollController.addListener(() async {
+//         if (newScrollController.position.pixels >=
+//             newScrollController.position.maxScrollExtent - 200) {
+//           controller.loadDataOnScroll();
+//         }
+//       });
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.white,
+//       body: SafeArea(
+//         child: RefreshIndicator(
+//           onRefresh: () => controller.loadData(),
+//           child: CustomScrollView(
+//             controller: newScrollController,
+//             slivers: [
+//               ProfileSection(),
+//                SliverGap(24),
+
+//               // Tabs
+//               SliverPersistentHeader(
+//                 // pinned: true,
+//                 // floating: false,
+//                 delegate: _SliverTabBarDelegate(
+//                   TabBar(
+//                     controller: tabController,
+//                     labelColor: Colors.black,
+//                     unselectedLabelColor: Colors.grey,
+//                     indicatorColor: Theme.of(context).primaryColor,
+//                     tabs: const [
+//                       Tab(text: 'All'),
+//                       Tab(text: 'Attendance'),
+//                       Tab(text: 'Collections'),
+//                       Tab(text: 'Files'),
+//                     ],
+//                   ),
+//                 ),
+//               ),
+
+//               // Tab content
+//               SliverToBoxAdapter(
+//                 child: SizedBox(
+//                   height: MediaQuery.of(context).size.height * 0.8,
+//                   child: TabBarView(
+//                     controller: tabController,
+//                     children: [
+//                       // Tab 1: All posts
+//                       _buildPostsList(),
+//                       // Tab 2: Attendance
+//                       Center(
+//                         child: Text(
+//                           'Attendance Section',
+//                           style: TextStyle(fontSize: 18),
+//                         ),
+//                       ),
+//                       // Tab 3: Collections
+//                       Center(
+//                         child: Text(
+//                           'Collections Section',
+//                           style: TextStyle(fontSize: 18),
+//                         ),
+//                       ),
+//                       // Tab 4: Files
+//                       Center(
+//                         child: Text(
+//                           'Files Section',
+//                           style: TextStyle(fontSize: 18),
+//                         ),
+//                       ),
+//                     ],
+//                   ),
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+
+//   // Helper to build the "All" posts list
+//   Widget _buildPostsList() {
+//     return GetBuilder<PostController>(
+//       builder: (postcontroller) {
+//         return ListView.builder(
+//           physics: const NeverScrollableScrollPhysics(),
+//           shrinkWrap: true,
+//           itemCount: postcontroller.posts.length,
+//           itemBuilder: (context, index) {
+//             Post post = postcontroller.posts[index];
+//             return PostWidget(
+//               post: post,
+//               onEdit: () {
+//                 // controller.selectItemAndNavigateToUpdatePage(post);
+//               },
+//               onDelete: () async {
+//                 // controller.delete(post.id!);
+//               },
+//             );
+//           },
+//         );
+//       },
+//     );
+//   }
+
+//   @override
+//   void dispose() {
+//     tabController.dispose();
+//     newScrollController.dispose();
+//     super.dispose();
+//   }
+// }
+
+// // Delegate for SliverPersistentHeader to make the TabBar sticky
+// class _SliverTabBarDelegate extends SliverPersistentHeaderDelegate {
+//   final TabBar tabBar;
+
+//   _SliverTabBarDelegate(this.tabBar);
+
+//   @override
+//   Widget build(
+//       BuildContext context, double shrinkOffset, bool overlapsContent) {
+//     return Container(
+//       color: Colors.white,
+//       child: tabBar,
+//     );
+//   }
+
+//   @override
+//   double get maxExtent => tabBar.preferredSize.height;
+
+//   @override
+//   double get minExtent => tabBar.preferredSize.height;
+
+//   @override
+//   bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
+//     return true;
+//   }
+// }
+
+
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
-import 'package:geolocation/core/api/dio/api_service.dart';
-import 'package:geolocation/core/globalwidget/sliver_gap.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:gap/gap.dart';
+import 'package:geolocation/core/globalwidget/images/online_image.dart';
+import 'package:geolocation/core/globalwidget/to_sliver.dart';
 import 'package:geolocation/core/theme/palette.dart';
+import 'package:geolocation/features/home/all_tab.dart';
+import 'package:geolocation/features/home/attendance_tab.dart';
+import 'package:geolocation/features/home/collections_tab.dart';
 import 'package:geolocation/features/home/dashboard/widget/profile_section.dart';
-import 'package:geolocation/features/post/controller/post_controller.dart';
-import 'package:geolocation/features/post/model/post.dart';
-import 'package:geolocation/features/post/widget/post_card.dart';
-import 'package:geolocation/features/post/widget/post_widget.dart';
+import 'package:geolocation/features/home/files_tab.dart';
+import 'package:geolocation/features/home/posts_tab.dart';
+
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
-import 'package:get/get_state_manager/src/simple/get_state.dart';
-
-
 class OfficerHomePage extends StatefulWidget {
-const OfficerHomePage({ Key? key }) : super(key: key);
+  const OfficerHomePage({ Key? key }) : super(key: key);
 
   @override
-  State<OfficerHomePage> createState() => _OfficerHomePageState();
+  _OfficerHomePageState createState() => _OfficerHomePageState();
 }
 
-class _OfficerHomePageState extends State<OfficerHomePage> {
 
-   final PostController controller = Get.find<PostController>();
-  final ScrollController newScrollController = ScrollController();
+class _OfficerHomePageState extends State<OfficerHomePage>
+    with SingleTickerProviderStateMixin {
+  final ScrollController scrollController = ScrollController();
+  late TabController _tabController;
 
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 5, vsync: this);
 
-   @override
-   void initState() {
-     super.initState();
-     WidgetsBinding.instance.addPostFrameCallback((_) {
-       controller.loadData();
-      // if (controller.posts.isEmpty) {
-       
-      // }
+    // Add a listener to detect tab index changes
+    _tabController.addListener(() {
+      // Ensure the listener reacts only when the tab has finished changing
+      if (_tabController.index != _tabController.previousIndex) {
+        print('Tab changed to index: ${_tabController.index}');
+      }
+    });
 
-      newScrollController.addListener(() async {
-        if (newScrollController.position.pixels >=
-            newScrollController.position.maxScrollExtent - 200) {
-          controller.loadDataOnScroll();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      scrollController.addListener(() {
+        if (scrollController.position.pixels ==
+            scrollController.position.maxScrollExtent) {
+          print('End of Scroll');
         }
       });
     });
-
-   }
+  }
 
   @override
-  Widget build(BuildContext context){
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: RefreshIndicator(
-          triggerMode: RefreshIndicatorTriggerMode.anywhere,
-        onRefresh: () => controller.loadData(),
-        child: SafeArea(
-          child: CustomScrollView(
-        controller: newScrollController,
-          shrinkWrap: true,
-          physics:  AlwaysScrollableScrollPhysics(),
-            slivers: [
-              ProfileSection(),
-              SliverGap(24),
-        
-               GetBuilder<PostController>(
-                 builder: (postcontroller) {
-                   return SliverAlignedGrid.count(
-                                crossAxisSpacing: 8,
-                                mainAxisSpacing: 8,
-                                itemCount: postcontroller.posts.length,
-                                crossAxisCount: 2,
-                                itemBuilder: (context, index) {
-                                  Post post = postcontroller.posts[index];
-        
-                                   return PostCard(post: post); // Display PostWidget
-        
-                                });
-                 }
-               )
-        
-            ],
-          ),
+      body: SafeArea(
+        child: CustomScrollView(
+          controller: scrollController,
+          slivers: [
+            ProfileSection(),
+            SliverGap(24),
+            SliverPadding(
+              padding: EdgeInsets.symmetric(
+                horizontal: 16,
+              ),
+              sliver: ToSliver(
+                child: Container(
+                  height: 60,
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 60, // Explicit height
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: 10,
+                          itemBuilder: (context, index) {
+                            return Stack(
+                              children: [
+                                Container(
+                                  decoration: BoxDecoration(
+                                      border: Border.all(
+                                          width: 4, color: Palette.PRIMARY),
+                                      borderRadius: BorderRadius.circular(60)),
+                                  margin: EdgeInsets.only(right: 16),
+                                  height: 60,
+                                  width: 60,
+                                  child: OnlineImage(
+                                    imageUrl: 'https://picsum.photos/200/300',
+                                    borderRadius: BorderRadius.circular(50),
+                                  ),
+                                ),
+                              ],
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SliverGap(24),
+            SliverPadding(
+              padding: const EdgeInsets.only(left: 16),
+              sliver: ToSliver(
+                child: TabBar(
+                    padding: EdgeInsets.all(0),
+                    tabAlignment: TabAlignment.start,
+                    dividerColor: Colors.transparent,
+                    isScrollable: true,
+                    controller: _tabController,
+                    labelColor: Colors.white,
+                    unselectedLabelColor: Palette.LIGHT_PRIMARY,
+                    indicatorSize: TabBarIndicatorSize.tab,
+                    indicator: BoxDecoration(
+                      borderRadius: BorderRadius.circular(4),
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                          Palette.PRIMARY,
+                          Palette.DARK_PRIMARY,
+                        ],
+                      ),
+                    ),
+                    tabs: [
+                      Tab(
+                        child: Text(
+                          "All".toUpperCase(),
+                        ),
+                      ),
+                      Tab(
+                        child: Text(
+                          "ATTENDANCE".toUpperCase(),
+                        ),
+                      ),
+                      Tab(
+                        child: Text(
+                          "POST".toUpperCase(),
+                        ),
+                      ),
+                      Tab(
+                        child: Text(
+                          "COLLECTIONS".toUpperCase(),
+                        ),
+                      ),
+                      Tab(
+                        child: Text(
+                          "FILES".toUpperCase(),
+                        ),
+                      ),
+                    ]),
+              ),
+            ),
+            SliverFillRemaining(
+              child: Container(
+                height: 300,
+                child: TabBarView(
+                  controller: _tabController,
+                  children: [
+                    AllTab(),
+                    AttendanceTab(),
+                    PostsTab(),
+                    CollectionsTab(),
+                    FilesTab(),
+                  ],
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
   }
+
 }
--- lib/features/post/widget/post_card.dart
+++ lib/features/post/widget/post_card.dart
@@ -1,63 +1,237 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
-import 'package:geolocation/core/theme/palette.dart';
+import 'package:gap/gap.dart';
+import 'package:geolocation/features/collections/model/collection.dart';
+import 'package:geolocation/features/post/widget/collection_chart_widget.dart';
+import 'package:get/get.dart';
+
+import 'package:geolocation/core/globalwidget/images/online_image.dart';
+import 'package:geolocation/core/globalwidget/images/online_image_full_screen_display.dart';
+import 'package:geolocation/core/globalwidget/preview_image.dart';
+import 'package:geolocation/core/globalwidget/preview_video.dart';
+import 'package:geolocation/features/file/model/media_file.dart';
 import 'package:geolocation/features/post/model/post.dart';
+import 'package:geolocation/features/video/online_video_player.dart';
+import 'package:geolocation/features/video/video_player_widget.dart';
 
 class PostCard extends StatelessWidget {
   final Post post;
 
-  const PostCard({Key? key, required this.post}) : super(key: key);
+    final VoidCallback onEdit;
+  final VoidCallback onDelete;
+  const PostCard({
+    Key? key,
+    required this.post,
+    required this.onEdit,
+    required this.onDelete,
+  }) : super(key: key);
+
+
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 3,
-      margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-      shape: RoundedRectangleBorder(
+    return Container(
+      padding: const EdgeInsets.all(12.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
         borderRadius: BorderRadius.circular(12),
+       
       ),
-      color: Palette.BACKGROUND, // Apply custom theme background
-      child: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // Post Title
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Header with title and timestamp
+          _buildHeader(),
+
+          const SizedBox(height: 8),
+
+          // Post Content
+          if (post.content != null && post.content!.isNotEmpty)
             Text(
-              post.title ?? "Untitled Post",
-              style: Theme.of(context).textTheme.titleLarge, // Use AppTheme's titleLarge
+              post.content!,
+              style: const TextStyle(fontSize: 16, color: Colors.black87),
+            ),
+
+          const SizedBox(height: 8),
+
+          // Media Grid (Images/Videos)
+          if (post.media != null && post.media!.isNotEmpty)
+            _buildMediaGrid(post.media!),
+         
+
+          const SizedBox(height: 12),
+
+          if (post.relatedModel != null &&
+              post.relatedModel!.type == 'Collection' &&
+              post.relatedModel!.data is Collection) ...[
+            const Text(
+              'Collection Chart:',
+              style: TextStyle(fontWeight: FontWeight.bold),
             ),
             const SizedBox(height: 8),
-
-            // Post Description
-            if (post.description != null && post.description!.isNotEmpty)
-              Text(
-                post.description!,
-                style: Theme.of(context).textTheme.bodyMedium, // Use AppTheme's bodyMedium
-              ),
-
-            const SizedBox(height: 8),
-
-            // Created Date or Publishing Status
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                if (post.createdAt != null)
-                  Text(
-                    "Created: ${post.createdAt}",
-                    style: Theme.of(context).textTheme.bodySmall, // Use AppTheme's bodySmall
-                  ),
-                if (post.isPublish != null)
-                  Text(
-                    post.isPublish! ? "Published" : "Unpublished",
-                    style: Theme.of(context).textTheme.bodySmall?.copyWith(
-                          color: post.isPublish! ? Colors.green : Colors.red,
-                        ),
-                  ),
-              ],
+            CollectionChartWidget(
+              collection: post.relatedModel!.data as Collection,
             ),
           ],
-        ),
+        
+
+          // Footer (removed Like button)
+        ],
       ),
     );
   }
+
+  Widget _buildHeader() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          height: 60,
+          width: 60,
+          
+          child: OnlineImage(imageUrl: '${post.councilPosition?.image}',borderRadius: BorderRadius.circular(40),),
+        ),
+        Gap(8),
+        // Title and Timestamp
+        Flexible(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                post.title ?? 'Untitled Post',
+                style: Get.textTheme.bodyLarge?.copyWith(
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              const SizedBox(height: 4),
+              Text(
+                post.createdAt ?? 'Just now',
+                style: const TextStyle(fontSize: 14, color: Colors.grey),
+              ),
+            ],
+          ),
+        ),
+
+        // Menu Icon
+        // IconButton(
+        //   icon: const Icon(Icons.more_vert),
+        //   onPressed: () {
+        //     // Add menu actions (edit/delete) if needed
+        //   },
+        // ),
+
+         PopupMenuButton<String>(
+                icon: const Icon(Icons.more_vert),
+                onSelected: (value) {
+                  if (value == 'edit') {
+                    onEdit();
+                  } else if (value == 'delete') {
+                    onDelete();
+                  }
+                },
+                itemBuilder: (context) => [
+                  const PopupMenuItem(
+                    value: 'edit',
+                    child: ListTile(
+                      leading: Icon(Icons.edit),
+                      title: Text('Edit'),
+                    ),
+                  ),
+                  const PopupMenuItem(
+                    value: 'delete',
+                    child: ListTile(
+                      leading: Icon(Icons.delete),
+                      title: Text('Delete'),
+                    ),
+                  ),
+                ],
+              ),
+      ],
+    );
+  }
+
+  Widget _buildMediaGrid(List<MediaFile> mediaFiles) {
+    int mediaCount = mediaFiles.length;
+
+    // Determine grid layout based on the number of media files
+    if (mediaCount == 1) {
+      return _buildSingleMedia(mediaFiles[0]);
+    } else if (mediaCount == 2) {
+      return Row(
+        children: mediaFiles.map((file) {
+          return Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: _buildMedia(file),
+            ),
+          );
+        }).toList(),
+      );
+    } else if (mediaCount == 3) {
+      return Column(
+        children: [
+          _buildSingleMedia(mediaFiles[0]),
+          const SizedBox(height: 4),
+          Row(
+            children: mediaFiles.sublist(1).map((file) {
+              return Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: _buildMedia(file),
+                ),
+              );
+            }).toList(),
+          ),
+        ],
+      );
+    } else {
+      // For 4+ media files
+      return GridView.builder(
+        physics: const NeverScrollableScrollPhysics(),
+        shrinkWrap: true,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 4,
+          mainAxisSpacing: 4,
+        ),
+        itemCount: mediaCount,
+        itemBuilder: (context, index) {
+          return _buildMedia(mediaFiles[index]);
+        },
+      );
+    }
+  }
+
+  Widget _buildSingleMedia(MediaFile file) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(12),
+      child: _buildMedia(file),
+    );
+  }
+Widget _buildMedia(MediaFile file) {
+  return GestureDetector(
+    onTap: () {
+      // Navigate to the full-screen view page based on media type
+      if (MediaFile.imageFormats.contains(file.extension)) {
+        // Navigate to the image viewer
+        Get.to(()=> OnlineImageFullScreenDisplay(imageUrl: file.url ?? ''),);
+       
+      } else if (MediaFile.videoFormats.contains(file.extension)) {
+        // Navigate to the video player
+        Get.to(()=> OnlineVideoPlayer(url: file.url ??''));
+        
+      }
+    },
+    child: MediaFile.imageFormats.contains(file.extension)
+        ? PreviewImage(url: file.url ?? '', height: 160)
+        : MediaFile.videoFormats.contains(file.extension)
+            ? PreviewVideo(file: file, height: 160)
+            : Center(
+                child: Icon(Icons.file_present, size: 40, color: Colors.grey),
+              ),
+  );
+}
+
+
 }
